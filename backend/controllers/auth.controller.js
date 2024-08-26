438d24a9f50ed5adbc9cfbf2655faecd3eb013df
toancyto09 <101265301+toancyto09@users.noreply.github.com>
2024-08-26 23:42:17 +0700

    1-authe controller added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,18 +1,115 @@
+import bcrypt from 'bcryptjs';
+import User from '../models/user.model.js'
+import { generateTokenAndSetCookie } from '../lib/untils/generateToken.js';
+
+
+//singup controller
 export const signup = async (req, res) => {
-  res.json({
-    data: "You hit the signup endpoint"
-  });
+  try {
+    const { fullname, username, email, password } = req.body;
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "User is already taken" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email is already taken" });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({ error: "Password must be at least 6 characters long" });
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullname,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      return res.status(201).json({
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
+      })
+    } else {
+      return res.status(400).json({ error: "invalid user data " });
+    };
+  } catch (error) {
+    console.log("Error is signup controller", error.message);
+    return res.status(500).json({ error: "Interval Server Erorr" });
+  }
+
+
 };
 
 export const login = async (req, res) => {
-  res.json({
-    data: "You hit the login endpoint"
-  });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    };
+
+    generateTokenAndSetCookie(user._id, res);
+
+    return res.status(200).json({
+      _id: user._id,
+      fullname: user.fullname,
+      username: user.username,
+      email: user.email,
+      followers: user.followers,
+      following: user.following,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg,
+    });
+
+  } catch (error) {
+    console.log("Error is login controller", error.message);
+    return res.status(500).json({ error: "Interval Server Erorr" });
+  }
 };
 
 
 export const logout = async (req, res) => {
-  res.json({
-    data: "You hit the logout endpoint"
-  });
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    return res.status(200).json({ message: "Logged out successfull" });
+  } catch (error) {
+    console.log("Error is logout controller", error.message);
+    return res.status(500).json({ error: "Interval Server Erorr" });
+  }
+};
+
+export const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log("Error is logout controller", error.message);
+    return res.status(500).json({ error: "Interval Server Erorr" });
+  }
 };
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { login, logout, signup } from '../controllers/auth.controller.js';
+import { getMe, login, logout, signup } from '../controllers/auth.controller.js';
+import { protecRoute } from '../middleware/protecRoute.js';
 const router = express.Router();
 
+router.post("/me", protecRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
