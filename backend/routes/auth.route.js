883f883df04607036b03fa6891886bea8de56f0d
toancyto09 <101265301+toancyto09@users.noreply.github.com>
2024-09-04 23:43:45 +0700

    3- update auth route and anth handled and routes protected in clinet

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -3,7 +3,7 @@ import { getMe, login, logout, signup } from '../controllers/auth.controller.js'
 import { protecRoute } from '../middleware/protecRoute.js';
 const router = express.Router();
 
-router.post("/me", protecRoute, getMe);
+router.get("/me", protecRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
+
 import HomePage from "./pages/home/HomePage"
 import SignUpPage from "./pages/auth/signup/SignUpPage"
 import LoginPage from "./pages/auth/login/LoginPage"
@@ -6,18 +7,53 @@ import Sidebar from "./components/common/Sidebar"
 import RightPanel from "./components/common/RightPanel"
 import Notifications from "./pages/notification/NotificationPage"
 import ProfilePage from "./pages/profile/ProfilePage"
+
+import { Toaster } from "react-hot-toast"
+import { useQuery } from "@tanstack/react-query"
+import LoadingSpinner from "./components/common/LoadingSpinner"
+
 function App() {
+  const { data: authUser, isLoading } = useQuery({
+    // we use queryKey to give a unique name to our query and refer to it later
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+        if (data.error) return null;
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        console.log("authUser is here:", data);
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry: false,
+  });
+
+  if (isLoading) {
+    return (
+      <div className='h-screen flex justify-center items-center'>
+        <LoadingSpinner size='lg' />
+      </div>
+    );
+  }
+
+
   return (
     <div className="flex max-w-6xl mx-auto">
-      <Sidebar />
+      {authUser && <Sidebar />}
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/notifications' element={<Notifications />} />
-        <Route path='/profile/:username' element={<ProfilePage />} />
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+        <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
+        <Route path='/notifications' element={authUser ? <Notifications /> : <Navigate to='/login' />} />
+        <Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
       </Routes>
-      <RightPanel />
+      {authUser && <RightPanel />}
+      <Toaster />
     </div>
   )
 }
